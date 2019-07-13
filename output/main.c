a2bc8e236c6530385225a95f2abb8390ed15a1e2
Akira Moroo <retrage01@gmail.com>
2019-07-13 14:27:00 +0900

    Add output with inline asm for external call
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

---
+++ output/main.c
@@ -0,0 +1,15 @@
+#include "efi.h"
+
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+  CHAR16 *String = L"Hello, EBC!\n";
+
+  asm("pushn %1\n" // String
+      "pushn %0\n" // SystemTable
+      "movnw %0, @%0 (5,24)\n" // SystemTable->ConOut
+      "call32exa @%0 (1,0)\n" // ConOut->OutputString
+      "movqw r0, r0 (2,0)\n"
+      ::"r"(SystemTable), "r"(String):);
+
+  return EFI_SUCCESS;
+}
