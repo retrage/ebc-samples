879a4cea8331d9cf54b3b844bc745e3354c77225
Akira Moroo <retrage01@gmail.com>
2019-07-14 00:40:47 +0900

    Fix 06_callputc test inline assembly
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- test/06_callputc.c
+++ test/06_callputc.c
@@ -2,9 +2,9 @@
 
 void output_string(EFI_SYSTEM_TABLE *SystemTable, CHAR16 *String)
 {
-  asm("pushn %1\n" // String
-      "pushn %0\n" // SystemTable
-      "movnw %0, @%0 (5,24)\n" // SystemTable->ConOut
+  asm("movnw %0, @%0 (5,24)\n" // SystemTable->ConOut
+      "pushn %1\n" // String
+      "pushn %0\n" // ConOut
       "call32exa @%0 (1,0)\n" // ConOut->OutputString
       "movqw r0, r0 (2,0)\n"
       ::"r"(SystemTable), "r"(String):);
